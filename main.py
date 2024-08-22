92914533b68ef1be7790c164c8f11b893060ca8d
Vlad Vorontsov <vo_vorontsov@betboom.org>
2024-08-23 03:40:52 +0400

    added host

--- main.py
+++ main.py
@@ -156,4 +156,5 @@ class EditLink(Resource):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=8080)
+    port = 8080
+    app.run(debug=True, host='0.0.0.0', port=port)
